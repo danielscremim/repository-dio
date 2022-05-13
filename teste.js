20ab070616e6e97d441d471718d69ecfb17d482e
danielscremim <danielsouzascremim@hotmail.com>
2022-05-13 17:54:37 -0300

    teste de soma

---
+++ teste.js
@@ -0,0 +1,17 @@
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a = parseInt(lines.shift());
+var b = parseInt(lines.shift());
+   
+console.log('X = ' + (a+b));
+
+/*
+var a = 10;
+var b = 9;
+
+let x = (a + b)
+
+console.log(`X = ${x}`)
+*/
