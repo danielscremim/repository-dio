31dc9489c3e5331ed443b3bf14b8abe8faaa1187
Daniel Souza Scremim <danielsouzascremim@hotmail.com>
2022-06-09 22:02:30 -0300

    DOM

---
+++ PRATICE/atividades-práticas/DOM class/assets/js/scripts.js
@@ -0,0 +1,11 @@
+function changeMode() {
+	changeClasses();
+	changeText();
+}
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
